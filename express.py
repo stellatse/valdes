cc0fca63501249fa68841a8e488bf28720507aa1
Stella <stellatsecyin@gmail.com>
2013-12-04 16:50:11 +0800

    test again

--- express.py
+++ express.py
@@ -41,25 +41,28 @@ class signup:
         return render.signup()
 		
     def POST(self):
-        # try:
-        i = web.input()
-        data = web.data()
-        print i
-        print data
-        u = User(name='', fullname='', password=i.password, email=i.email, phone=i.phone, address='', section=0)
-        web.ctx.orm.add(u)
-        # except Exception, e:
-            # return json.dumps(["reg_error"])
-        # else:
-        if u.id:
-            web.setcookie('uid', str(u.id), settings.COOKIE_EXPIRES)
-            raise web.seeother('/user/%d' % u.id)
+        try:
+            i = web.input()
+            data = web.data()
+            print i
+            print data
+            u = User(name='', fullname='', password=i.password, email=i.email, phone=i.phone, address='', section=0)
+            web.ctx.orm.add(u)
+        except Exception, e:
+            return json.dumps(["reg_error"])
+        else:
+            if u.id:
+                web.setcookie('uid', str(u.id), settings.COOKIE_EXPIRES)
+                raise web.seeother('/user/%d' % u.id)
 	
 class login:
     def GET(self):
         return render.login()
 
-
+class account:
+    def GET(self, user_id):
+        print user_id
+        return render.login()
 		
 if __name__ == "__main__":
     app.run()
